9c6e67f8b524f02df4dbf03fe5a69e97c4c583b1
dank0 <ohw0sseug@gmail.com>
2020-04-29 19:33:04 +0600

    updated/appended tests

---
+++ tests/__init__.py
@@ -0,0 +1,36 @@
+import logging
+import os
+import unittest
+
+try:
+    import dlib
+except ImportError:
+    dlib = None
+else:
+    from face_engine import fetching
+
+    # fetch if dlib installed
+    fetching.fetch_models()
+    fetching.fetch_images()
+    fetching.fetch_datasets()
+
+from face_engine import RESOURCES
+
+
+class TestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        images = os.path.join(RESOURCES, 'images')
+        cls.bubbles1 = os.path.join(images, 'bubbles1.jpg')
+        cls.bubbles2 = os.path.join(images, 'bubbles2.jpg')
+        cls.cat = os.path.join(images, 'cat.jpg')
+        cls.dog = os.path.join(images, 'dog.jpg')
+        cls.drive = os.path.join(images, 'drive.jpg')
+        cls.family = os.path.join(images, 'family.jpg')
+        cls.train = os.path.join(RESOURCES, 'datasets', 'train')
+        cls.test = os.path.join(RESOURCES, 'datasets', 'test')
+
+    @classmethod
+    def tearDownClass(cls):
+        logging.disable(logging.NOTSET)
---
+++ tests/test_face_engine_models.py
@@ -0,0 +1,154 @@
+import unittest
+
+import numpy as np
+
+from extra.dataset import load_dataset
+from face_engine.exceptions import FaceNotFoundError
+from face_engine.models import Detector, Embedder, Estimator, BasicEstimator
+from face_engine.tools import imread
+from tests import TestCase, dlib
+
+if dlib:
+    from face_engine.models.dlib_models import (
+        HOGDetector, MMODDetector, ResNetEmbedder)
+
+
+class TestDetector(TestCase):
+    """Test cases to test detectors.
+
+    To use this tests @override setUp() method with your detector
+
+    Note that this abstract class is not collected by unittest.
+    """
+
+    def setUp(self):
+        self.detector = Detector()
+
+    def test_detect_one_return_data_is_tuple(self):
+        data = self.detector.detect_one(imread(self.bubbles1))
+        self.assertIsNotNone(data)
+
+    def test_detect_one_return_bb_is_tuple(self):
+        bb = self.detector.detect_one(imread(self.bubbles1))[1]
+        self.assertIsInstance(bb, tuple)
+
+    def test_detect_one_return_one_bb(self):
+        bb = self.detector.detect_one(imread(self.bubbles1))[1]
+        self.assertEqual(len(bb), 4)
+
+    def test_detect_one_raises_face_not_found_error(self):
+        with self.assertRaises(FaceNotFoundError):
+            self.detector.detect_one(imread(self.cat))
+
+    def test_detect_all_return_data_is_tuple(self):
+        data = self.detector.detect_all(imread(self.bubbles1))
+        self.assertIsNotNone(data)
+
+    def test_detect_all_return_bbs_is_tuple(self):
+        bbs = self.detector.detect_all(imread(self.bubbles1))[1]
+        for bb in bbs:
+            self.assertIsInstance(bb, tuple)
+
+    def test_detect_all_return_multiple_bbs(self):
+        bbs = self.detector.detect_all(imread(self.family))[1]
+        self.assertEqual(len(bbs), 3)
+
+    def test_detect_all_raises_face_not_found_error(self):
+        with self.assertRaises(FaceNotFoundError):
+            self.detector.detect_all(imread(self.cat))
+
+
+@unittest.skipUnless(dlib, "dlib package is not installed")
+class TestHOGDetector(TestDetector):
+
+    def setUp(self):
+        self.detector = HOGDetector()
+
+
+@unittest.skipUnless(dlib, "dlib package is not installed")
+class TestMMODDetector(TestDetector):
+
+    def setUp(self):
+        self.detector = MMODDetector()
+
+
+class TestEmbedder(TestCase):
+    """Test cases to test embedders.
+
+    To use this tests @override setUp() method with your embedder
+
+    Note that this abstract class is not collected by unittest.
+    """
+
+    def setUp(self):
+        self.image = imread(self.bubbles1)
+        self.bbs = [(278, 132, 618, 471)]
+        self.embedder = Embedder()
+
+    def test_compute_embedding_return_data_is_numpy_array(self):
+        data = self.embedder.compute_embedding(self.image, self.bbs[0])
+        self.assertIsInstance(data, np.ndarray)
+
+    def test_compute_embedding_return_data_shape(self):
+        data = self.embedder.compute_embedding(self.image, self.bbs[0])
+        self.assertEqual(data.shape, (self.embedder.embedding_dim,))
+
+    def test_compute_embeddings_return_data_is_numpy_array(self):
+        data = self.embedder.compute_embeddings(self.image, self.bbs)
+        self.assertIsInstance(data, np.ndarray)
+
+    def test_compute_embeddings_return_data_shape(self):
+        data = self.embedder.compute_embeddings(self.image, self.bbs)
+        self.assertEqual(data.shape, (1, self.embedder.embedding_dim))
+
+
+@unittest.skipUnless(dlib, "dlib package is not installed")
+class TestResNetEmbedder(TestEmbedder):
+
+    def setUp(self):
+        super().setUp()
+        self.embedder = ResNetEmbedder()
+
+
+class TestEstimator(TestCase):
+    """Test cases to test estimators.
+
+    To use this tests @override setUp() method with your estimator
+
+    Note that this abstract class is not collected by unittest.
+    """
+
+    def setUp(self):
+        self.estimator = Estimator()
+        train_dataset = load_dataset(self.train)
+        self.train_emb = train_dataset[3]
+        self.train_names = train_dataset[1]
+        test_dataset = load_dataset(self.test)
+        self.test_emb = test_dataset[3]
+        self.test_names = test_dataset[1]
+
+    def test_fit_with_defaults(self):
+        self.estimator.fit(np.array(self.train_emb), self.train_names)
+        # fit completes without troubles
+        self.assertTrue(True)
+
+    def test_predict_test_dataset(self):
+        self.estimator.fit(np.array(self.train_emb), self.train_names)
+        names = self.estimator.predict(np.array(self.test_emb))[1]
+        self.assertEqual(self.test_names, names)
+
+
+class TestBasicEstimator(TestEstimator):
+
+    def setUp(self):
+        super().setUp()
+        self.estimator = BasicEstimator()
+
+
+# remove abstract tests
+del TestDetector
+del TestEmbedder
+del TestEstimator
+
+if __name__ == '__main__':
+    unittest.main()
